3d6f677df92634519e93e5235b8f1b4fd5f4a349
Edivan <75508556+buged@users.noreply.github.com>
2022-02-26 14:35:30 -0300

    Added git clone and update

--- update.sh
+++ update.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
+updateSystem () {
 cd /home/us3r/nixos
 git pull https://github.com/buged/nixos
 sudo cp /home/us3r/nixos/configuration.nix /etc/nixos/
 nixos-rebuild switch
+}
+
+cloneRepo () {
+# Cloning the repository
+git clone https://github.com/buged/nixos
+# Giving execute permission to the update.sh file
+sudo chmod a+x /home/us3r/nixos/update.sh
+}
+
+FILE=/home/us3r/nixos/configuration.nix
+if [ -f "$FILE" ]; then
+    updateSystem
+else 
+    cloneRepo
+		updateSystem
+fi
