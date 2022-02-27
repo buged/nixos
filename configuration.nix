3dff3273990845d77182c7206a1bb2791dbe7034
Edivan <75508556+buged@users.noreply.github.com>
2022-02-27 15:32:27 -0300

    enable wifi automatic connection

--- configuration.nix
+++ configuration.nix
@@ -13,11 +13,12 @@
   networking.networkmanager.enable = true;
   networking.hostName = "nixos"; # Define your hostname.
   networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
-  networking.wireless.networks = {
-  "." = {                # SSID with no spaces or special characters
-    psk = "bfe5ba93ebcad0b18e2164cf5d5bb0e9f241bfb5af477164a95022477de02b3e";
-  };
-  };
+  networking.wireless.networks.".".pskRaw = "bfe5ba93ebcad0b18e2164cf5d5bb0e9f241bfb5af477164a95022477de02b3e";
+  # networking.wireless.networks = {
+  # "." = {                # SSID with no spaces or special characters
+  #   psk = "bfe5ba93ebcad0b18e2164cf5d5bb0e9f241bfb5af477164a95022477de02b3e";
+  # };
+  # };
 
   # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
