b9165e5a1665c5b25f27d6e45c8fa405af759d32
Edivan <75508556+buged@users.noreply.github.com>
2022-02-28 19:44:40 -0300

    minor changes

--- configuration.nix
+++ configuration.nix
@@ -97,20 +97,13 @@
     duf                  # disk usage/free utility
     # simple-scan          # scanner gui
     # simplescreenrecorder # screen recorder gui
-    # xclip # Tool to access the X clipboard from a console application
-    # neovim
-    # alacritty
-    
-    # Enable vim copy/past external
-    # xorg.libX11
-    # xorg.libXt
     
     # sys tools
     wget
     htop
     
     # TODO:
-    vim_configurable
+    vim
     p7zip
     qbittorrent
     flameshot
